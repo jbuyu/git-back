e496b36209c1b7f0529a7b0d8e1cf807e9879960
jbuyu <jbuyu@ymail.com>
2021-02-11 22:01:13 +0300

    fixes posting route, adds body parser for json

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 4000;
 const app = express();
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 //middleware
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //mongoose
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -24,8 +28,9 @@ app.get("/", (req, res) => {
   res.send("Howdy!");
 });
 
-app.post("/", (req, res) => {
-  let { repoDat } = req.body;
+app.post("/repos", (req, res) => {
+  // let { newRepoData } = req.body;
+  console.log(req.body);
 });
 
 app.listen(PORT, () => console.log("Listening on port 3000!"));
