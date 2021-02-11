55d53a0dab0db9018f37e5618f5030eef961b844
jbuyu <jbuyu@ymail.com>
2021-02-11 23:33:52 +0300

    bug fix

--- server.js
+++ server.js
@@ -60,13 +60,13 @@ app.post("/repos", (req, res) => {
 });
 
 app.get("/favRepos", async (req, res) => {
-  const repos = await Repo.find();
-  var result = repos.reduce((unique, o) => {
-    if (!unique.some((obj) => obj.label === o.label && obj.value === o.value)) {
-      unique.push(o);
-    }
-    return unique;
-  }, []);
+  const result = await Repo.find();
+  // var result = repos.reduce((unique, o) => {
+  //   if (!unique.some((obj) => obj.label === o.label && obj.value === o.value)) {
+  //     unique.push(o);
+  //   }
+  //   return unique;
+  // }, []);
   res.send({
     result,
   });
