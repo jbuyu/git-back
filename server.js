25b77e99f026aa7722e57c738ab8f187b4eb7f36
jbuyu <jbuyu@ymail.com>
2021-02-11 22:42:15 +0300

    check if object exists already

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
 
 //middleware
 app.use(cors());
@@ -23,14 +24,39 @@ connection.once("open", function () {
   console.log("MongoDB database connection established successfully");
 });
 
+//schema
+
+const repoSchema = new Schema({
+  owner: String,
+  repoName: { type: String, required: true },
+  issues: String,
+  forks: Number,
+  stars: Number,
+});
+
+const Repo = model("Repo", repoSchema);
+
 //routes
 app.get("/", (req, res) => {
   res.send("Howdy!");
 });
 
 app.post("/repos", (req, res) => {
-  // let { newRepoData } = req.body;
-  console.log(req.body);
+  let newRepoData = req.body;
+  let repoData = new Repo({
+    ...newRepoData,
+  });
+  let repo = Repo.exists({ repoName: newRepoData.repoName });
+  if (!repo) {
+    repoData.save(function (error) {
+      console.log("saved");
+      if (error) {
+        console.error(error);
+      }
+    });
+  } else {
+    console.log("already exists");
+  }
 });
 
-app.listen(PORT, () => console.log("Listening on port 3000!"));
+app.listen(PORT, () => console.log("Listening on port 4000!"));
